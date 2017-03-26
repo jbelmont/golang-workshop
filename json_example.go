eb36afb7f79f0c1cc74323edf72962a7733df346
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-03-26 11:17:25 -0400

    Add json examples in golang.

--- json_example.go
+++ json_example.go
@@ -2,6 +2,8 @@ package workshop
 
 import (
 	"encoding/json"
+	"fmt"
+	"reflect"
 	"strconv"
 )
 
@@ -32,4 +34,78 @@ func jsonExamples() {
 	convertTypeString := string(typeString[:])
 	convertExpected := "Program"
 	assert(convertTypeString != convertExpected)
+
+	slice := []string{"jack", "be", "nimble"}
+	typeSlice, err := json.Marshal(slice)
+
+	var parsed []interface{}
+	err2 := json.Unmarshal(typeSlice, &parsed)
+	if err2 != nil {
+		panic(err2)
+	}
+	var expectedSlice = []string{"jack", "be", "nimble"}
+	var sliceCompare = true
+	for idx, val := range parsed {
+		if val != expectedSlice[idx] {
+			sliceCompare = false
+		}
+	}
+	assert(sliceCompare == true)
+
+	data := []byte(`
+{
+	"names": ["James", "Jane", "Chen"],
+	"hash": {
+		"secret": "12345abcdef"
+	}
+}
+	`)
+	var parsed2 map[string]interface{}
+	err3 := json.Unmarshal(data, &parsed2)
+	if err3 != nil {
+		panic(err3)
+	}
+	for idx, value := range parsed2 {
+		switch value.(type) {
+		case []interface{}:
+			expectedNestedSlice := map[string]interface{}{
+				"names": []string{"James", "Jane", "Chen"},
+				"hash":  map[string]interface{}{"secret": "12345abcdef"},
+			}
+			_, ok := value.([]interface{})
+			_, ok2 := expectedNestedSlice["names"].([]interface{})
+			assert(ok != ok2)
+		case map[string]interface{}:
+			fmt.Println(idx, value)
+			fmt.Println(reflect.TypeOf(value))
+		default:
+			fmt.Println("no matches")
+		}
+	}
+
+	type Slices struct {
+		Names []string               `json:"names"`
+		Hash  map[string]interface{} `json:"hash"`
+	}
+	var knownJSON Slices
+	err4 := json.Unmarshal(data, &knownJSON)
+	if err4 != nil {
+		panic(err4)
+	}
+
+	namesSlice := knownJSON.Names
+	var namesSliceExpected = []string{"James", "Jane", "Chen"}
+	assertSlices := true
+	for idx, vals := range namesSlice {
+		if vals != namesSliceExpected[idx] {
+			assertSlices = false
+		}
+	}
+	assert(assertSlices == true)
+
+	actualSecret := knownJSON.Hash["secret"]
+	secretHash := map[string]string{
+		"secret": "12345abcdef",
+	}
+	assert(actualSecret == secretHash["secret"])
 }
