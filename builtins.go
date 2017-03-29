41ca45d382f1a0a304be1171f9eaa2793a76e468
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-03-28 20:51:59 -0400

    Some examples of builtins.

--- builtins.go
+++ builtins.go
@@ -1,5 +1,23 @@
 package workshop
 
+import "fmt"
+
 func builtins() {
-	
+	var slice1 = []string{
+		"I am a string",
+		"I am the other string",
+	}
+	slice := append(slice1, "Just another string")
+	sliceFinal := make([]string, len(slice))
+	copy(sliceFinal, slice)
+	comparator := func(slice, slicefinal []string) bool {
+		fmt.Println(slice, sliceFinal)
+		for i := range slice {
+			if slice[i] != slicefinal[i] {
+				return false
+			}
+		}
+		return true
+	}
+	assert(comparator(slice, sliceFinal) == true)
 }
