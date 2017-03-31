7bb736c8dbbe822ff7bc97b6feab9903f5e5efc3
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-03-30 20:34:57 -0400

    Add exercise for closures.

--- practice/closures.go
+++ practice/closures.go
@@ -1,13 +1,35 @@
 package practice
 
-func averageEmUp(numbers []int) int {
+import (
+	"math"
+	"strconv"
+)
+
+func averageEmUp(numbers []float64) float64 {
 	return average(numbers)
 }
 
-func average(numbers []int) int {
-	sum := 0
+func average(numbers []float64) float64 {
+	sum := 0.0
 	for i := 0; i < len(numbers); i++ {
 		sum += numbers[i]
 	}
-	return sum / len(numbers)
+	return sum / float64(len(numbers))
+}
+
+func format2DecimalPlaces(num float64) string {
+	str := strconv.FormatFloat(num, 'f', 2, 64)
+	return str
+}
+
+func standardDeviation(numbers []float64) func() string {
+	mean := average(numbers)
+	var summation float64
+	return func() string {
+		for i := 0; i < len(numbers); i++ {
+			summation += math.Pow(math.Abs((numbers[i] - mean)), 2)
+		}
+		format2Dec := format2DecimalPlaces(math.Sqrt(summation / float64(len(numbers))))
+		return format2Dec
+	}
 }
--- practice/closures_test.go
+++ practice/closures_test.go
@@ -5,11 +5,27 @@ import (
 )
 
 func TestAverage(t *testing.T) {
-	numbers := []int{
+	numbers := []float64{
 		1, 2, 3, 4, 5, 6, 7,
 	}
 	addEmUp := averageEmUp(numbers)
 	if addEmUp != 4 {
 		t.Error("Expected 4 but received ", addEmUp)
+	}
+}
+
+func TestStandarDeviation(t *testing.T) {
+	numbers := []float64{
+		5.3,
+		2.5,
+		3.75,
+		9.55,
+		8.5,
+		6.75,
+	}
+	actual := standardDeviation(numbers)()
+	expected := "2.49"
+	if actual != expected {
+		t.Error("Expected 2.49 but received ", actual)
 	}
 }
